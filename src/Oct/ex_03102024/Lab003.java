3ec1aef9e34366627eb8a9579deb3cb808ccc210
Rohit Sawant - CitiusTech <rohit.sawant@mediware.com>
2024-10-03 20:24:32 -0500

    Initial commit

---
+++ src/Oct/ex_03102024/Lab003.java
@@ -0,0 +1,20 @@
+package Oct.ex_03102024;
+
+public class Lab003 {
+    public static void main(String[] args){
+        System.out.println("Simple calculator");
+
+        System.out.println("Addition");
+        System.out.println("10+2=12");
+
+        System.out.println("Subtraction");
+        System.out.println("15-10=5");
+
+        System.out.println("Multiplication");
+        System.out.println("10*5=50");
+
+        System.out.println("Division");
+        System.out.println("30/2=15");
+
+    }
+}
