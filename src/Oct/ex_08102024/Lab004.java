e4311a6b96f56962daf2f9eb1eb1f8aff73c3d1f
Snehal Sawant <snehal_65@yahoo.in>
2024-10-08 23:51:32 -0500

    practise database type

---
+++ src/Oct/ex_08102024/Lab004.java
@@ -0,0 +1,22 @@
+package Oct.ex_08102024;
+
+public class Lab004 {
+    public static void main(String[] args) {
+        System.out.println("Snehal'age now");
+
+        int age = 65;
+        age =  age+1;
+        age = 76;
+        System.out.println(age);
+        int _= 23;
+        int snehal = 45;
+        int a = 24;
+        boolean is_female = true;
+        boolean is_married = true;
+        boolean is_learning_java_class = true;
+        byte b  = 126;
+        long phone_number_of_snehal = 12345566l;
+
+
+    }
+}
