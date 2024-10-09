f5b96e13fcc32983b7e9a5191c8fc65eb4313608
Snehal Sawant <snehal_65@yahoo.in>
2024-10-09 01:44:09 -0500

    Java practise of printf

---
+++ src/Oct/ex_09102024/Lab005.java
@@ -0,0 +1,27 @@
+package Oct.ex_09102024;
+
+public class Lab005 {
+    public static void main(String[] args) {
+        int i = 5;
+        System.out.println("Table of 5");
+        System.out.printf("%d * %d = %d",i,1,i*1);
+        System.out.println();
+        System.out.printf("%d * %d = %d",i,2,i*2);
+        System.out.println();
+        System.out.printf("%d * %d = %d",i,3,i*3);
+        System.out.println();
+        System.out.printf("%d * %d = %d",i,4,i*4);
+        System.out.println();
+        System.out.printf("%d * %d = %d",i,5,i*5);
+        System.out.println();
+        System.out.printf("%d * %d = %d",i,6,i*6);
+        System.out.println();
+        System.out.printf("%d * %d = %d",i,7,i*7);
+        System.out.println();
+        System.out.printf("%d * %d = %d",i,8,i*8);
+        System.out.println();
+        System.out.printf("%d * %d = %d",i,9,i*9);
+        System.out.println();
+        System.out.printf("%d * %d = %d",i,10,i*10);
+    }
+}
