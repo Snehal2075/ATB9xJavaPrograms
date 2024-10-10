d205db39bcbbc24923ce0bdd6068f743cb2da3f8
Snehal Sawant <snehal_65@yahoo.in>
2024-10-10 00:24:29 -0500

    Literals.java

---
+++ src/Oct/ex_10102024/Lab006.java
@@ -0,0 +1,72 @@
+package Oct.ex_10102024;
+
+public class Lab006 {
+    public static void main(String[] args) {
+
+//     ****** BINARY LITERAL*******
+//      1.  binary literal for decimal 10
+        int bin1 = 0b1010;
+//      2.  binary literal for decimal 13
+        int bin2 = 0B1101;
+//       3. binary literal for decimal 255 (8 bits)
+        int bin3 = 0b11111111;
+
+//      ********INTEGER LITERAL*******
+//        1. Decimal literals
+        int decimal = 10;
+//        2.Binary literal (prefix 0b or 0B)
+      int binary = 0b1010;
+//       3. Octal literal (prefix 0)
+        int octal = 012;
+//        Hexadecimal literal (prefix 0x or 0X)
+
+//        *********FLOATING LITERAL******
+//        1.Floating-point literal (suffix 'f' or 'F')
+        float floatliteral = 10.5f;
+//        2.Double-precision floating-point literal (default type)
+        double doubleliteral = 10.5;
+//        3.Exponential notation (1.05 * 10^2)
+        double expliteral = 1.05e2;
+
+//        *********CHARACTER LITERAL*******
+//        1.Character Literal
+        char ch1 = 'A';
+//        2. Unicode representation (A)
+         char ch2 = '\u5AF3';
+//         3. Escape sequence
+         char ch3 = '\n';
+
+//         ******* STRING LITERAL ********
+//      1. String literal
+        String str1 = "My name is Snehal";
+//        2. String containing numbers
+        String str2 = "14567";
+
+
+//        ******* BOOLEAN LITERAL*********
+//        1. Boolean literal= TRUE
+        boolean snehal_is_learning_Java = true;
+        boolean Snehal_is_learning_Python = false;
+
+//        ****** NULL LITERAL ********
+        String str = null;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
