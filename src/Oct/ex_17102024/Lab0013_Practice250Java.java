2e64749f90a4869adf204a84263d2a61efff1c1a
Snehal Sawant <snehal_65@yahoo.in>
2024-10-17 15:28:21 -0500

    Practise Questions All files

---
+++ src/Oct/ex_17102024/Lab0013_Practice250Java.java
@@ -0,0 +1,40 @@
+package Oct.ex_17102024;
+
+import java.util.Scanner;
+
+public class Lab0013_Practice250Java {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the score(0-100)");
+
+        int score = scanner.nextInt();
+
+        char grade;
+
+        if (score <= 100 && score >=90) {
+            System.out.println("The grade is A");
+
+        } else if (score <= 89 && score >= 80) {
+            System.out.println("The grade is B");
+
+        } else if (score <= 79 && score >= 60) {
+            System.out.println("The grade is C");
+
+        } else if (score <= 59 && score >= 50) {
+            System.out.println("The grade is D");
+
+        } else
+        {
+            System.out.println("The grade is F");
+
+            System.out.println("The letter grade is + :" + score );
+        }
+        scanner.close();
+
+
+        }
+    }
+
---
+++ src/Oct/ex_17102024/Lab0014_TriangleQ.java
@@ -0,0 +1,32 @@
+package Oct.ex_17102024;
+
+import java.util.Scanner;
+
+public class Lab0014_TriangleQ {
+    public static void main(String[] args) {
+
+        // Classify the Triangle on basis of its side lengths
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the value of side1:");
+        double side1 = scanner.nextDouble();
+        System.out.println("Enter the value of side2");
+        double side2 = scanner.nextDouble();
+        System.out.println("Enter the value of side3");
+        double side3 = scanner.nextDouble();
+
+        if(side1==side2 && side2==side3) {
+            System.out.println("It is equilateral Triangle");
+
+        } else if (side1==side2 && side2!=side3) {
+            System.out.println("It is Isoceles Triangle");
+
+        }else
+            System.out.println("It is Scalene Triangle");
+
+        scanner.close();
+
+
+
+    }
+}
